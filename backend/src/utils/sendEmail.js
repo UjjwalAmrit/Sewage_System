20c0a6908fe59b788bd3953f08ce59024aa53f17
UjjwalAmrit <ujjwalamrit54321@gmail.com>
2025-08-18 18:28:56 +0530

    Correction in the feature of contact form

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -2,7 +2,7 @@ const nodemailer = require("nodemailer")
 
 const sendEmail = async ({ to, subject, html }) => {
   try {
-    const transporter = nodemailer.createTransporter({
+    const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
         user: process.env.EMAIL_USER,
