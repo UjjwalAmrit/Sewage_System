b4091547b0b84919637e2f2f6d0c27b552a3522a
Jaygautam04 <152092407+Jaygautam04@users.noreply.github.com>
2025-09-14 19:10:08 +0530

    Update AboutUs.jsx

--- backend/frontend/src/pages/AboutUs.jsx
+++ backend/frontend/src/pages/AboutUs.jsx
@@ -36,7 +36,7 @@ const AboutUs = () => {
       {/* Hero Section */}
       <section
         className="relative text-white py-24 overflow-hidden bg-cover bg-center"
-        style={{ backgroundImage: "url('./about-us.jpg')" }} // ✅ path fixed
+        style={{ backgroundImage: "url('/about-us.jpg')" }} // ✅ path fixed
       >
         {/* Dark overlay so text is visible */}
         <div className="absolute inset-0 bg-black/50"></div>
