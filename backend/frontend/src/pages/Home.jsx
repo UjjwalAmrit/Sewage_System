52de975f5e6039484c54b632fba7c5fe33689839
Jaygautam04 <152092407+Jaygautam04@users.noreply.github.com>
2025-12-23 21:51:26 +0530

    Update Home.jsx

--- backend/frontend/src/pages/Home.jsx
+++ backend/frontend/src/pages/Home.jsx
@@ -71,12 +71,7 @@ const Home = () => {
             >
               Our Services <FaArrowRight className="ml-2" />
             </a>
-            <a
-              href="/contact-us"
-              className="border-2 bg-green-500 border-white text-white px-8 py-3 rounded-lg font-semibold hover:bg-white hover:text-purple-900 transition-colors shadow-md hover:shadow-lg"
-            >
-              Get Quote
-            </a>
+           
           </div>
         </div>
       </div>
